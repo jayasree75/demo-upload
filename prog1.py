c6198cdab604c1d4cdefabfc1f9be792a47fc0fc
jayasree75 <84917944+jayasree75@users.noreply.github.com>
2021-05-27 19:38:18 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,16 @@
+#comments
+print("hello mani")
+#type
+a=12
+b=2.1
+c= True
+d='mani'
+print(type(a),type(b),type(c),type(d))
+#expressions
+add=a+b
+print(add)
+div=a//b
+print(div)
+#type casting
+typecast=int(b)
+print(typecast)
